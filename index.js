b18702bc9dac47a85e02552b09772bb4de9612d1
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-10 15:21:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase)
   return "I can't hear you!"
   var uppercase = "HELLO!"
-  if (uppercase.toUpperCase() === uppercase)
+  elseif (uppercase.toUpperCase() === uppercase)
   return "YES INDEED!"
   var mixedCase = 'Hi there!'
-  if (string = "I love you, Grandma.")
+  elseif (string = "I love you, Grandma.")
   return "I love you, too."
 }
